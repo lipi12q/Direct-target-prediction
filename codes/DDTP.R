9e2c45ea19c4999b90598a72ccc9dd0fef4acff8
Peng Li <30860375+lipi12q@users.noreply.github.com>
2023-06-26 17:22:04 +0800

    Update DDTP.R

--- codes/DDTP.R
+++ codes/DDTP.R
@@ -1,13 +1,13 @@
 
 
-#DTPP evaluating drug-direct target interaction by DDS which is determined by logit model by combining DETP socre (DES) and DBTP model score (BS) 
+#DTPP evaluating drug-direct target interaction by DDS which is determined by logit model by combining DETP socre (DES) and DBTP model score (DBS) 
 
-model<-glm(label~DES+BS,data = train_data, family = binomial(link ="logit"))
+model<-glm(label~DES+DBS,data = train_data, family = binomial(link ="logit"))
 #
-summary(model_1)
+summary(model)
 #prediction
 fitted.prob<-predict(model, newdata = test_data, type = "response")
-test_data$DDS<-model_1$fitted.values
+test_data$DDS<-model$fitted.values
  
 
 
